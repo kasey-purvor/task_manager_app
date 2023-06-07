f26effa5ae09356f7a9c7de059e42e8e9dc966e8
kasey purvor <kasey.purvor@gmail.com>
2023-06-07 14:33:04 +0100

    changed backened index.js to use conditional logic for port and ip depending on dev or prod environments

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,12 @@
 const server = require('./app')
 const port = process.env.PORT 
-const ip = process.env.BACKEND_IP
-server.listen(port, ip, () => {
-    console.log(`Backend server is running on port ${port}`)
-})
+if (process.env.DEV ==="true") {
+    const ip = process.env.BACKEND_IP
+    server.listen(port, ip, () => {
+        console.log(`Backend server is running on port ${port}`)
+    })
+} else {
+    server.listen(port, () => {
+        console.log(`Backend server is running on port ${port}`)
+    })
+}
