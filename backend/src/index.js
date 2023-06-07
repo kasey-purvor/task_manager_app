4df1ba07eb811ea63d3acb3c6bdb5f32cc768827
kasey purvor <kasey.purvor@gmail.com>
2023-06-07 17:16:49 +0100

    attempt to get docker container to run the API. removed logic regarding https connection.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,15 @@
 const server = require('./app')
 const port = process.env.PORT 
-if (process.env.DEV ==="true") {
-    const ip = process.env.BACKEND_IP
-    server.listen(port, ip, () => {
-        console.log(`Dev environment, Backend server is running on port ${port}`)
-    })
-} else {
-    server.listen(port, () => {
-        console.log(`PROD environment, Backend server is running on port ${port}`)
-    })
-}
+// if (process.env.DEV ==="true") {
+//     const ip = process.env.BACKEND_IP
+//     server.listen(port, ip, () => {
+//         console.log(`Dev environment, Backend server is running on port ${port}`)
+//     })
+// } else {
+//     server.listen(port, () => {
+//         console.log(`PROD environment, Backend server is running on port ${port}`)
+//     })
+// }
+server.listen(port, () => {
+    console.log(`Backend server is running on port ${port}`)
+})
