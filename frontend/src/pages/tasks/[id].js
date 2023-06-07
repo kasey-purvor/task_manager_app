3db8bac4c528c61e94ceb4a6210a228a6c0907c3
kasey purvor <kasey.purvor@gmail.com>
2023-06-07 17:05:57 +0100

    turned off frontend API calls

--- frontend/src/pages/tasks/[id].js
+++ frontend/src/pages/tasks/[id].js
@@ -4,31 +4,31 @@ import { useState } from 'react';
 
 import TaskEditForm from '@/components/taskForm';
 import PageContainer from '@/components/pageContainer';
-export async function getStaticPaths() {
-    const allTaskIds = await getAllTaskIds();
-    return {
-        paths: allTaskIds,
-        fallback: false,
-    };
-}
-export async function getStaticProps({ params }) {
-    const taskData = await getTask(params.id);
-    return {
-        props: {
-            taskData,
-        },
-    };
-}
+// export async function getStaticPaths() {
+//     const allTaskIds = await getAllTaskIds();
+//     return {
+//         paths: allTaskIds,
+//         fallback: false,
+//     };
+// }
+// export async function getStaticProps({ params }) {
+//     const taskData = await getTask(params.id);
+//     return {
+//         props: {
+//             taskData,
+//         },
+//     };
+// }
 
 export default function TaskDetailsPage({ taskData }) {
 
     return (
         <PageContainer>    
                
-            <TaskEditForm
+            {/* <TaskEditForm
                 taskData={taskData}
                 formEdit={true}
-            />
+            /> */}
 
         </PageContainer>
     );
