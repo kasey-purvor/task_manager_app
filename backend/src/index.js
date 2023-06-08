c232128ee2f3eb7ed90ff5a0b226553be942c747
kasey purvor <kasey.purvor@gmail.com>
2023-06-08 13:39:41 +0100

    removed conditional code from index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,6 @@
 const server = require('./app')
 const port = process.env.PORT 
-if (process.env.NEXT_PUBLIC_DEV ==="true") {
-    const ip = process.env.BACKEND_IP
-    server.listen(port, ip, () => {
-        console.log(`Dev environment, Backend server is running on port ${port}`)
-    })
-} else {
-    server.listen(port, () => {
-        console.log(`PROD environment, Backend server is running on port ${port}`)
-    })
-}
-server.listen(process.env.PORT, () => {
+
+server.listen(port, () => {
     console.log(`Backend server is running on port ${port}`)
 })
