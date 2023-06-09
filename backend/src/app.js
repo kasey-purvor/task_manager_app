49849ffe534648b8faf0aa838ffdd66df45f0a82
kasey purvor <kasey.purvor@gmail.com>
2023-06-09 16:27:29 +0100

    cleanout of unnecessary code

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,20 +6,12 @@ const cookieParser = require("cookie-parser");
 const https = require("https");
 // const {createProxyMiddleware} = require("http-proxy-middleware");
 const fs = require("fs");
-const cors = require('cors');
-const path = require('path');
-
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT;
-console.log("This is the backened ROOT", process.cwd())
-
-if (process.env.DEV === "true") {
-    
-} else {
-    var options = {}
-}
-
+console.log("This is the backened ROOT", process.cwd());
 
 app.options("/tasks/:id", (req, res, next) => {
     // Set CORS headers for preflight request
@@ -29,7 +21,6 @@ app.options("/tasks/:id", (req, res, next) => {
 
     // Respond with 204 No Content
     res.sendStatus(204);
-
 });
 // app.options('/users/', (req, res, next) => {
 //     res.header("Access-Control-Allow-Origin", "https://localhost:3001");
@@ -41,26 +32,16 @@ app.options("/tasks/:id", (req, res, next) => {
 //     res.header("Access-Control-Allow-Origin", "https://localhost:3001");
 //     res.header("Access-Control-Allow-Methods", "POST");
 //     res.header("Access-Control-Allow-Headers", "Authorization, Content-Type");
-  
+
 // })
 app.use(cors({
-    origin: "*", 
-    credentials: true,
-  }));
+        origin: "*",
+        credentials: true,
+    })
+);
 app.use(cookieParser());
 app.use(express.json()); // makes express auto parse JSON in the body of the request
 app.use(taskRouter);
 app.use(userRouter);
-// if(process.env.DEV === 'true') {
-//     var options = {
-//         key: fs.readFileSync("./certs/localhost-key.pem"),
-//         cert: fs.readFileSync("./certs/localhost.pem"),
-//     };
-//     const server = https.createServer(options, app)
-//     module.exports = server;
-// } else {
-    module.exports = app
-// }
 
-
-
+module.exports = app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,6 @@
 const server = require('./app')
 const port = process.env.PORT 
-// if (process.env.DEV ==="true") {
-//     const ip = process.env.BACKEND_IP
-//     server.listen(port, ip, () => {
-//         console.log(`Dev environment, Backend server is running on port ${port}`)
-//     })
-// } else {
-//     server.listen(port, () => {
-//         console.log(`PROD environment, Backend server is running on port ${port}`)
-//     })
-// }
+
 server.listen(3000, () => {
     console.log(`Backend server is running on port ${port}`)
 })
