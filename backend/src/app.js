b200b05e4371e76290dddd153198abbb35dcf684
kasey purvor <kasey.purvor@gmail.com>
2023-06-09 17:08:21 +0100

    navbar max width set to full

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,27 +13,27 @@ const app = express();
 const port = process.env.PORT;
 console.log("This is the backened ROOT", process.cwd());
 
-app.options("/tasks/:id", (req, res, next) => {
-    // Set CORS headers for preflight request
-    res.header("Access-Control-Allow-Origin", "https://localhost:3001");
-    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH");
-    res.header("Access-Control-Allow-Headers", "Authorization, Content-Type");
-
-    // Respond with 204 No Content
-    res.sendStatus(204);
-});
-// app.options('/users/', (req, res, next) => {
+// app.options("/tasks/:id", (req, res, next) => {
+//     // Set CORS headers for preflight request
 //     res.header("Access-Control-Allow-Origin", "https://localhost:3001");
-//     res.header("Access-Control-Allow-Methods", "GET, POST, DELETE,PATCH");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH");
 //     res.header("Access-Control-Allow-Headers", "Authorization, Content-Type");
 
-// })
-// app.options('/users/login', (req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "https://localhost:3001");
-//     res.header("Access-Control-Allow-Methods", "POST");
-//     res.header("Access-Control-Allow-Headers", "Authorization, Content-Type");
+//     // Respond with 204 No Content
+//     res.sendStatus(204);
+// });
+// // app.options('/users/', (req, res, next) => {
+// //     res.header("Access-Control-Allow-Origin", "https://localhost:3001");
+// //     res.header("Access-Control-Allow-Methods", "GET, POST, DELETE,PATCH");
+// //     res.header("Access-Control-Allow-Headers", "Authorization, Content-Type");
 
-// })
+// // })
+// // app.options('/users/login', (req, res, next) => {
+// //     res.header("Access-Control-Allow-Origin", "https://localhost:3001");
+// //     res.header("Access-Control-Allow-Methods", "POST");
+// //     res.header("Access-Control-Allow-Headers", "Authorization, Content-Type");
+
+// // })
 app.use(cors({
         origin: "*",
         credentials: true,
