a83e7297fe7886affab9dcee8d9fbd2691b274b0
kasey purvor <kasey.purvor@gmail.com>
2023-06-25 17:10:57 +0100

    testing vercel build

--- frontend/src/pages/api/[...proxy].js
+++ frontend/src/pages/api/[...proxy].js
@@ -1,102 +1,105 @@
-    import { createProxyMiddleware, responseInterceptor } from "http-proxy-middleware";
-    import Cookies from "cookies";
-    export const config = {
-        api: {
-            bodyParser: false,
-        },
-    };
-    if (process.env.NEXT_PUBLIC_DEV === 'true') {
-        var token = process.env.NEXT_PUBLIC_TOKEN_DEV
-    } else {
-        var token = NEXT_PUBLIC_TOKEN_PROD
-    }
-    const backendApiUrl = process.env.NEXT_PUBLIC_BACKEND_ADDRESS
-    const pathRewrite = (path, req) => {
-        if (req.url === "/api/signin") {
-            return path.replace("/api/signin", "/api/users/login");
-        } else if (req.url === "/api/signup") {
-            return path.replace("/api/signup", "/api/users");
-        } else if (req.url === "/api/allTasks") {
-            console.log("path rewrite", path)
-            return path.replace("/api/allTasks", "/api/tasks");
-        }
-    };
-    const proxyResHAndler = (proxyRes, req, res) => {
-        const pathName = req.url;
-        var userCookieEdit = false
-        if(pathName === "/api/users" || pathName === "/api/users/login") {
-            userCookieEdit = true
-        } 
-        var data = [];
-        proxyRes.on("data", (chunk) => {
-            console.log("concating cata")
-            data.push(chunk);
-            console.log("data response", data)
-        });
-        
-        proxyRes.on("end", async () => {
-            try {
-                const dataJSON = JSON.parse(Buffer.concat(data).toString("utf-8"));
-                // console.log(" returned from API", dataJSON);
-                const cookies = new Cookies(req, res);
-                if (userCookieEdit) {
-                    console.log("setting cookie to client")
-                    cookies.set("auth-token", dataJSON.token, {
-                        httpOnly: false,
-                        sameSite: "lax",
-                        // domain: "http://localhost",
-                        path: "/"
-                    });
-                }
-            // const tasks = JSON.parse(data.toString())
-                res.send(dataJSON);
-                return ;
-            } catch (e) {
-                res.send(e);
-            }
-        });
-    };
+import { createProxyMiddleware, responseInterceptor } from "http-proxy-middleware";
+import Cookies from "cookies";
 
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
+
+if (process.env.NEXT_PUBLIC_DEV === 'true') {
+    var token = process.env.NEXT_PUBLIC_TOKEN_DEV
+} else {
+    var token = NEXT_PUBLIC_TOKEN_PROD
+}
+const backendApiUrl = process.env.NEXT_PUBLIC_BACKEND_ADDRESS
+
+const pathRewrite = (path, req) => {
+    if (req.url === "/api/signin") {
+        return path.replace("/api/signin", "/api/users/login");
+    } else if (req.url === "/api/signup") {
+        return path.replace("/api/signup", "/api/users");
+    } else if (req.url === "/api/allTasks") {
+        console.log("path rewrite", path)
+        return path.replace("/api/allTasks", "/api/tasks");
+    }
+};
+const proxyResHAndler = (proxyRes, req, res) => {
+    const pathName = req.url;
+    var userCookieEdit = false
+    if(pathName === "/api/users" || pathName === "/api/users/login") {
+        userCookieEdit = true
+    } 
+    var data = [];
+    proxyRes.on("data", (chunk) => {
+        console.log("concating cata")
+        data.push(chunk);
+        console.log("data response", data)
+    });
     
-
-    export default async function handler(req, res) {
-        // const pathName = req.url;
-        // const oldPath = "^" + pathName;
-        // if (pathName === "/api/signin") {
-        //     const newPath = "/api/users/login";
-        // } else if (pathName === "/api/signup") {
-        //     const newPath = "/api/users";
-        // }
-        const cookies = new Cookies(req, res);
-        var jwt = await cookies.get("auth-token");
-        console.log("have i got the http-cookies from the req?", jwt);
-        const proxy = createProxyMiddleware({   
-            target: "http://localhost:3000",
-            autoRewrite: false,
-            changeOrigin: true,
-            selfHandleResponse: true,
-            headers: {
-                "auth-token": token
-            },
-            pathRewrite: pathRewrite,
-            // onProxyReqWs: async (proxyReq, req, socket, options, head) => {
-            //     const cookies = new Cookies(req, {});
-            //     console.log(cookies)
-            //     const jwt = await cookies.get("auth-token");
-            //     console.log("have i got the http-cookies from the req?", jwt);
-            //     proxyReq.setHeader("auth-token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDc5YjZjMzg0NTUwM2NmNDE0OGVkMDEiLCJpYXQiOjE2ODU5NjYwNzF9.0FQ09fYPyZUUIlD-MaJvN29XkYquXhMKgV6HjqJQuqw");
-            //     console.log(proxyReq.headers)
-            //     return {options, head}
-            // },
-            onProxyRes: proxyResHAndler,
-        });
-        console.log("proxy call");
-        // req.setHeader("auth-token", jwt);
-        proxy(req, res, (err) => {
-            if (err) {
-                console.log("error called in the proxy call itself");
-                throw err;
+    proxyRes.on("end", async () => {
+        try {
+            const dataJSON = JSON.parse(Buffer.concat(data).toString("utf-8"));
+            // console.log(" returned from API", dataJSON);
+            const cookies = new Cookies(req, res);
+            if (userCookieEdit) {
+                console.log("setting cookie to client")
+                cookies.set("auth-token", dataJSON.token, {
+                    httpOnly: false,
+                    sameSite: "lax",
+                    // domain: "http://localhost",
+                    path: "/"
+                });
             }
-            throw new Error(`Request '${req.url}' is not proxied! We should never reach here!`);
-        });
-    }
+        // const tasks = JSON.parse(data.toString())
+            res.send(dataJSON);
+            return ;
+        } catch (e) {
+            res.send(e);
+        }
+    });
+};
+
+
+
+export default async function handler(req, res) {
+    // const pathName = req.url;
+    // const oldPath = "^" + pathName;
+    // if (pathName === "/api/signin") {
+    //     const newPath = "/api/users/login";
+    // } else if (pathName === "/api/signup") {
+    //     const newPath = "/api/users";
+    // }
+    const cookies = new Cookies(req, res);
+    var jwt = await cookies.get("auth-token");
+    console.log("have i got the http-cookies from the req?", jwt);
+    const proxy = createProxyMiddleware({   
+        target: backendApiUrl,
+        autoRewrite: false,
+        changeOrigin: true,
+        selfHandleResponse: true,
+        headers: {
+            "auth-token": token
+        },
+        pathRewrite: pathRewrite,
+        // onProxyReqWs: async (proxyReq, req, socket, options, head) => {
+        //     const cookies = new Cookies(req, {});
+        //     console.log(cookies)
+        //     const jwt = await cookies.get("auth-token");
+        //     console.log("have i got the http-cookies from the req?", jwt);
+        //     proxyReq.setHeader("auth-token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDc5YjZjMzg0NTUwM2NmNDE0OGVkMDEiLCJpYXQiOjE2ODU5NjYwNzF9.0FQ09fYPyZUUIlD-MaJvN29XkYquXhMKgV6HjqJQuqw");
+        //     console.log(proxyReq.headers)
+        //     return {options, head}
+        // },
+        onProxyRes: proxyResHAndler,
+    });
+    console.log("proxy call");
+    // req.setHeader("auth-token", jwt);
+    proxy(req, res, (err) => {
+        if (err) {
+            console.log("error called in the proxy call itself");
+            throw err;
+        }
+        throw new Error(`Request '${req.url}' is not proxied! We should never reach here!`);
+    });
+}
