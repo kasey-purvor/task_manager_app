57f4d258a15cc7ff8e5a96309270699bd39f5b33
kasey purvor <kasey.purvor@gmail.com>
2023-06-25 16:38:33 +0100

    attempting to remove the [id[ page to see if vercel will build

--- frontend/src/pages/tasks/[id].js
+++ frontend/src/pages/tasks/[id].js
@@ -1,35 +1,35 @@
-import { getAllTaskIds } from '@/utils/functions/tasksFunctions';
-import { getTask } from '@/utils/apiCalls/tasks/tasksApiCalls';
-import { useState } from 'react';
+// import { getAllTaskIds } from '@/utils/functions/tasksFunctions';
+// import { getTask } from '@/utils/apiCalls/tasks/tasksApiCalls';
+// import { useState } from 'react';
 
-import TaskEditForm from '@/components/taskForm';
-import PageContainer from '@/components/pageContainer';
-export async function getStaticPaths() {
-    const allTaskIds = await getAllTaskIds();
-    return {
-        paths: allTaskIds,
-        fallback: false,
-    };
-}
-export async function getStaticProps({ params }) {
-    const taskData = await getTask(params.id);
-    return {
-        props: {
-            taskData,
-        },
-    };
-}
+// import TaskEditForm from '@/components/taskForm';
+// import PageContainer from '@/components/pageContainer';
+// export async function getStaticPaths() {
+//     const allTaskIds = await getAllTaskIds();
+//     return {
+//         paths: allTaskIds,
+//         fallback: false,
+//     };
+// }
+// export async function getStaticProps({ params }) {
+//     const taskData = await getTask(params.id);
+//     return {
+//         props: {
+//             taskData,
+//         },
+//     };
+// }
 
-export default function TaskDetailsPage({ taskData }) {
+// export default function TaskDetailsPage({ taskData }) {
 
-    return (
-        <PageContainer>    
+//     return (
+//         <PageContainer>    
                
-            <TaskEditForm
-                taskData={taskData}
-                formEdit={true}
-            />
+//             <TaskEditForm
+//                 taskData={taskData}
+//                 formEdit={true}
+//             />
 
-        </PageContainer>
-    );
-}
+//         </PageContainer>
+//     );
+// }
