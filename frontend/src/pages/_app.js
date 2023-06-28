08d4ad6a1d3fff170f30cafc6a2c1dd83edbd8f4
kasey purvor <kasey.purvor@gmail.com>
2023-06-28 15:38:50 +0100

    added error handling react component to wrap around the _app page

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic'
 import '../styles/globals.css'
 import Navbar from '@/components/navbar'
+import ErrorBoundary from '@/components/errorBoundary'
 // import ('preline')
 dynamic(() => import('preline'), {
     ssr: false
@@ -9,7 +10,9 @@ export default function App({ Component, pageProps }) {
     return (
         <div className='bg-gray-300 h-full'>
             <Navbar/>
-            <Component {...pageProps} />
+            <ErrorBoundary>
+                <Component {...pageProps} />
+            </ErrorBoundary>
         </div>
     )
 }
