c74e08d9ea4f7597971711c555d0262a3767d30b
kasey purvor <kasey.purvor@gmail.com>
2023-06-28 15:26:14 +0100

    added react error handling

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,15 +1,18 @@
-import dynamic from 'next/dynamic'
-import '../styles/globals.css'
-import Navbar from '@/components/navbar'
+import dynamic from "next/dynamic";
+import "../styles/globals.css";
+import Navbar from "@/components/navbar";
+import ErrorBoundary from "@/components/errorBoundary";
 // import ('preline')
-dynamic(() => import('preline'), {
-    ssr: false
-})
+dynamic(() => import("preline"), {
+    ssr: false,
+});
 export default function App({ Component, pageProps }) {
     return (
-        <div className='bg-gray-300 h-full'>
-            <Navbar/>
-            <Component {...pageProps} />
+        <div className="bg-gray-300 h-full">
+            <Navbar />
+            <ErrorBoundary>
+                <Component {...pageProps} />
+            </ErrorBoundary>
         </div>
-    )
+    );
 }
