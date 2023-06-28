06d0eb8c02cc872f1bfa8fb943d88c4178d7eb60
kasey purvor <kasey.purvor@gmail.com>
2023-06-28 15:27:10 +0100

    Revert "added react error handling"
    This reverts commit c74e08d9ea4f7597971711c555d0262a3767d30b.

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,18 +1,15 @@
-import dynamic from "next/dynamic";
-import "../styles/globals.css";
-import Navbar from "@/components/navbar";
-import ErrorBoundary from "@/components/errorBoundary";
+import dynamic from 'next/dynamic'
+import '../styles/globals.css'
+import Navbar from '@/components/navbar'
 // import ('preline')
-dynamic(() => import("preline"), {
-    ssr: false,
-});
+dynamic(() => import('preline'), {
+    ssr: false
+})
 export default function App({ Component, pageProps }) {
     return (
-        <div className="bg-gray-300 h-full">
-            <Navbar />
-            <ErrorBoundary>
-                <Component {...pageProps} />
-            </ErrorBoundary>
+        <div className='bg-gray-300 h-full'>
+            <Navbar/>
+            <Component {...pageProps} />
         </div>
-    );
+    )
 }
