dea43b4dad8066b3c69a1abbef29d98fedd72f24
kasey purvor <kasey.purvor@gmail.com>
2023-07-04 13:19:17 +0100

    console.log for seeing if end of data streamevent is hit

--- frontend/src/proxy/proxy.js
+++ frontend/src/proxy/proxy.js
@@ -22,6 +22,7 @@ const proxyResHAndler = (proxyRes, req, res) => {
         console.log("data response", data);
     });
     proxyRes.on("end", () => {
+        console.log("end of data event hit ");
         try {
             const dataJSON = JSON.parse(Buffer.concat(data).toString("utf-8"));
             dataJSON ? console.log(" data returned from API") : null;
