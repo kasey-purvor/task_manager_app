476f8137b0cfbcfe1c83ab7049b7919547bc32b6
kasey-purvor <67878899+kasey-purvor@users.noreply.github.com>
2023-07-18 15:28:26 +0100

    Finishing touches  (#8)
    * changed backend wakeup call to be useEffect so that the homepage will render while the backend wakes up
    
    * added sequence diagram to homepage
    
    * updated homepage

--- frontend/src/components/homepage.js
+++ frontend/src/components/homepage.js
@@ -7,10 +7,13 @@ import {getAllTasks} from '@/utils/apiCalls/tasks/tasksApiCalls'
 const text = `# **Task Manager App**  
 ### *By Kasey Purvor*
 ### [Please see Github Page](https://github.com/kasey-purvor/task_manager_app)
-### ***Please Signup and create a task to try the CRUD operations. __Please be patient__ as the docker backend may have gone to sleep and may take a few seconds to start up - causing you first attempt to load the tasks page to timeout. A page refresh may be required.*** 
+### ***Please Signup and create a task. You can manage your tasks and account from the relevant pages. A simplified sequence diagram is sown below but this does not cover next.js rendering and re-rendering processes***
 ## **Project Description**
 
-#### What started as a node.js course, building a backend API, that I slowly completed part time - turned into a full stack project with a Next.JS / React frontend. Hence the rather large initial commit. It's fair to say this wont be winning any awards to artistic design. But the project has been a valuable exercise in bringing together a complex full stack project using the technologies I am interested in.
+#### What started as a backend API, that I slowly part time, turned into a full stack project with a Next.JS / React frontend.
+A user can create an account and create a digital "To Do" list with full CRUD control of their user and tasks. Authentication is done using JWT http-only cookies and a htp-proxy server. 
+
+It's fair to say this wont be winning any awards to artistic design. But the project has been a valuable exercise in bringing together a complex full stack project using the technologies I am interested in.
 ### **Technologies**
 * **Backend**
     * Node.js
@@ -27,8 +30,6 @@ const text = `# **Task Manager App**
     * Tailwind.css
     * Cookies
     
-### **Updates!** 
-* http-only cookies have been implmented and are now used for user identification. Now a user must first signup or login to perform CRUD operations to tasks.   
 ## **Structure**
 * ### **Backend**
 The Backend used node and express to manage the restful API. Mongoose is used to perform data sanitization and validation as well as handle the connections to a Mongo-Atlas DB. There are routes for CRUD operations for users and tasks. Emails are sent out when tasks are due and when users signup / cancel accounts. JSON webtokens are used for authentication and are verified using express middleware. Passwords are stored encrypted using bcrypt.
@@ -38,8 +39,6 @@ The Frontend was started more recently as I began learning how to use Next.js wi
 
 ## **Features to add**
 * Sorting features, to allow users to sort their tasks by their desired field
-* Conditional rendering of the navbar. So that login is not shown when user is logged in.
-* Account management page
 * Email notification settings for tasks
 * Many more I am sure I could spend years on but I likely move onto another project after this. 
 
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,58 +1,38 @@
 import Homepage from "@/components/homepage";
 const backendApiUrl = process.env.NEXT_PUBLIC_BACKEND_ADDRESS;
-const text = `# **Task Manager App**
-## By Kasey Purvor
-#### What started as a node.js course building a backend API, that I slowly completed part time, turned into something I wanted to finish and add additional features as well as a Next.JS / React front end. Hence the rather large initial commit containing most of the backened. I am not an experienced frontend developer, it's fair to say this wont be winning any awards to artistic design. But the project has been a valuable exersize in bringing together a full stack project using the technologies I am most interested in.
 
-## **Project Description**
-### **Backend**
-The Backened used node and express to manage the restful API. Mongoose is used to perform data sanitization and validation as well as handle the connections to a Mongo-Atlas DB. There are routes for CRUD operations for users and tasks. Emails are sent out when tasks are due and when users signup / cancel accounts. As well as the ability to upload a user avatar is desired. JSON webtokens are used for authentication and are verified using express middleware. Passwords are stored encrypted using bcrypt.
+import {useEffect, useState} from "react";
+import sequenceDiagram from './../../public/sequence_diagram.jpg'
 
-### **Frontend**
-The Frontend was started more recently as I began learning how to use Next.js with React, which i already had experience with. The frontend is server-side rendered and loads the tasks and user data from MongoDB, then loads them as React props before rendering and sending to the cient. There are still features to implement to compliment the backened routes I have implemented. I have used a proxy server to handle cookies, which I use to send JSONWebTokens for user authentication. 
-
-### **Technologies**
-* **Backend**
-    * Node.js
-    * Express
-    * Mongoose
-    * MongoDB & MongoDB Atlas
-    * bcrypt
-    * jwt
-* **Frontend**
-    * Next.js / Vercel
-    * React
-    * http-proxy
-    * Tailwind.css
-    * Cookies
-    
-## **Notes**
-I am in the process of implementing the use of http-only cookies to store the JSONwebtokens for authentication. With this i can then implement the login / logout features. Currently the cookies are saving to the client but not being recieved by my proxy server. This has proven quite difficult. 
-
-## **Features to add**
-* Sorting features, to allow users to sort their tasks by their desired field
-* Conditional rendering of the navbar. So that login is not shown when user is logged in.
-* Account management page
-* Email notification settings for tasks
-* Many more I am sure I could spend years on but I likely move onto another project after this. 
-
-`;
-export async function getServerSideProps() {
-    try {
-        console.log("Index page is waking up backend");
-        fetch(`${backendApiUrl}/api/allTasks`); // this is just a dummy call to wake up the backend when the homepage is loaded. no props are assigned. 
-        return {
-            props: {},
-        };
-    } catch (e) {
-        console.log(e);
-    }
-}
+export async function getStaticProps() {
+    // Get the path to the image file
+    console.log(sequenceDiagram);
+    // const imagePath = path.join(process.cwd(), 'public', 'my-image.jpg');
+  
+    // // Read the image file
+    // const imageData = fs.readFileSync(imagePath);
+  
+    // // Encode the image data as a base64 string
+    // const imageBase64 = imageData.toString('base64');
+  
+    // Pass the image data to the page component as a prop
+    return {
+      props: {
+        // imageBase64,
+      },
+    };
+  }
 
 export default function Home() {
+    useEffect(() => {
+        fetch(`${backendApiUrl}/api/allTasks`);
+    }, [])
     return (
         <main>
             <Homepage />
+            <div className=" bg-green-200 h-auto w-auto  flex justify-center py-6 sm:px-6 rounded-3xl sm:m-2 md:m-5">
+            <img src={sequenceDiagram.src} alt="Sequence Diagram"/>
+            </div>
         </main>
     );
 }
